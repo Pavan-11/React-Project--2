cf013193b9be55df9475fccfa2c134f5f555e8f3
Pavan-11 <121480038+Pavan-11@users.noreply.github.com>
2023-06-10 11:30:26 +0530

    updating college input in app.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import UsersList from './components/Users/UsersList';
 function App() {
   const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (uName, uAge) => {
+  const addUserHandler = (uName, uAge, uCollege) => {
     setUsersList((prevUsersList) => {
       return [
         ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
+        { name: uName, college: uCollege , age: uAge, id: Math.random().toString() }
       ];
     });
   };
